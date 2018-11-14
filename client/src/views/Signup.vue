df5bf914e8ce23c5b6df103dd98f1476cedcb957
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-11-14 09:53:33 +0000

    more on signup page

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,30 +1,36 @@
 <template>
 <div home>
 <div class="jumbotron">
-  <h1>Sign Up</h1>
+  <div class="text-center">
+    <h1>Sign Up</h1>
+  </div>
   <form>
     <div class="form-group">
       <label for="username">Username</label>
       <input type="text" class="form-control" id="username"
-        aria-describedby="usernameHelp" placeholder="Enter Username">
+        aria-describedby="usernameHelp" placeholder="Enter Username" required>
       <small id="usernameHelp" class="form-text text-muted">
         Username must be between 2 and 20 characters.
       </small>
     </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password"
-        aria-describedby="passswordHelp" placeholder="Password">
-      <small id="passwordHelp" class="form-text text-muted">
-        Password must be at least 8 characters.
-      </small>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password"
+          aria-describedby="passswordHelp" placeholder="Password" required>
+        <small id="passwordHelp" class="form-text text-muted">
+          Password must be at least 8 characters.
+        </small>
+      </div>
+      <div class="form-group col-md-6">
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" class="form-control" id="confirmPassword"
+          placeholder="Confirm Password" required>
+      </div>
     </div>
-    <div class="form-group">
-      <label for="confirmPassword">Confirm Password</label>
-      <input type="password" class="form-control" id="confirmPassword"
-        placeholder="Confirm Password">
+    <div class="text-center">
+      <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
     </div>
-    <button type="submit" class="btn btn-primary">Sign Up</button>
   </form>
 </div>
 </div>
