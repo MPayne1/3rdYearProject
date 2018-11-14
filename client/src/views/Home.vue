bae53590e68700d467c92c36d4d2eeba62e54020
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-11-14 09:39:37 +0000

    added signup page with username and password

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="home">
+  <div class="home text-center">
     <div class="jumbotron">
       <h1 class="display-3">SportsBook</h1>
-      <p class="lead">SportsBook is an easy to use web appliction to manage your amateur sports team.</p>
+      <p class="lead">SportsBook is an easy to use web application to
+        manage your amateur sports team.</p>
       <hr class="my-4">
       <p>Sign up and start today!</p>
       <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+        <router-link class="btn btn-primary btn-lg" :to="{name: 'signup'}"
+          role="button">Sign Up</router-link>
       </p>
     </div>
   </div>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,39 @@
+<template>
+<div home>
+<div class="jumbotron">
+  <h1>Sign Up</h1>
+  <form>
+    <div class="form-group">
+      <label for="username">Username</label>
+      <input type="text" class="form-control" id="username"
+        aria-describedby="usernameHelp" placeholder="Enter Username">
+      <small id="usernameHelp" class="form-text text-muted">
+        Username must be between 2 and 20 characters.
+      </small>
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password"
+        aria-describedby="passswordHelp" placeholder="Password">
+      <small id="passwordHelp" class="form-text text-muted">
+        Password must be at least 8 characters.
+      </small>
+    </div>
+    <div class="form-group">
+      <label for="confirmPassword">Confirm Password</label>
+      <input type="password" class="form-control" id="confirmPassword"
+        placeholder="Confirm Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Sign Up</button>
+  </form>
+</div>
+</div>
+</template>
+
+
+<script>
+export default {
+};
+</script>
+<style>
+</style>
