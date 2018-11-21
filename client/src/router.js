77c97cc94f3b3413da0c685e0474563b361d86d0
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-11-21 10:45:34 +0000

    user must be logged in, have token in local storage, to visit dashboard

--- client/src/router.js
+++ client/src/router.js
@@ -3,8 +3,27 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Signup from './views/Signup.vue';
 import Login from './views/Login.vue';
+import Dashboard from './views/Dashboard.vue';
 
 Vue.use(Router);
+
+// if user has a token theyre already logged in so redirect to dashboard
+function loggedInRedirectDashboard(to, from, next) {
+  if (localStorage.token) {
+    next('/dashboard');
+  } else {
+    next();
+  }
+}
+
+// if user is logged in, has a token, then let them continue
+function isLoggedIn(to, from, next) {
+  if(localStorage.token) {
+    next();
+  } else {
+    next('/login');
+  }
+}
 
 export default new Router({
   routes: [
@@ -17,11 +36,19 @@ export default new Router({
       path: '/Signup',
       name: 'signup',
       component: Signup,
+      beforeEnter: loggedInRedirectDashboard,
     },
     {
       path: '/Login',
       name: 'login',
       component: Login,
+      beforeEnter: loggedInRedirectDashboard,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      beforeEnter: isLoggedIn,
     },
   ],
 });
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home text-center">
+    <div class="jumbotron">
+      <h1 class="display-3">Dashboard</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'dashboard',
+  components: {
+  },
+};
+</script>
