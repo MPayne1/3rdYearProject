fb401263b019a6b41f9ad08bd5955347c23e0f53
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-12-20 10:08:22 +0000

    league search only returns leagues with room

--- Server/db/selectLeagueFromCity.js
+++ Server/db/selectLeagueFromCity.js
@@ -9,7 +9,9 @@ const dbCon  = require('./connection.js');
 */
 var selectLeagues  = async function(city, county, country, sport, callback) {
   var res;
-    var sql = `SELECT leagueID, LeagueName, Sport FROM League WHERE city = ${mysql.escape(city)} and county = ${mysql.escape(county)}  and country =  ${mysql.escape(country)} and Sport =  ${mysql.escape(sport)} ;`;
+    var sql = `SELECT LeagueName, Sport, League.leagueID, maxTeams FROM League, (select count(teamId) as noTeams, leagueId  from team group by leagueID) as teamNo
+      WHERE city = ${mysql.escape(city)} and county = ${mysql.escape(county)}  and country = ${mysql.escape(country)} and Sport = ${mysql.escape(sport)} and teamNo.noTeams <= League.MaxTeams and teamNo.LeagueID = League.LeagueID;
+ ;`;
 	  await dbCon.query(sql , (err, result, fields) => {
 		    if(err) throw err;
         res = result;
@@ -21,10 +23,12 @@ module.exports = selectLeagues;
 
 /*
 
-(SELECT LeagueName, Sport FROM League WHERE city = "Swansea" and county = "county"  and country = "country" and Sport = "sport") ;
-
 Select leagueName, foundLeagues.sport from
 (SELECT LeagueName, Sport, leagueID, maxTeams FROM League WHERE city = "Swansea" and county = "county"  and country = "country" and Sport = "sport") as foundLeagues,
 team where team.LeagueId = foundLeagues.LeagueID and count(teamName)> foundLeagues.maxTeams;
 
+no. of teams in a league: select count(teamId) as noTeams from team where LeagueID = 15;
+no of teams in each league: select count(teamId) as noTeams, leagueId  from team group by leagueID;
+only return leagues with room:  SELECT LeagueName, Sport, League.leagueID, maxTeams FROM League, (select count(teamId) as noTeams, leagueId  from team group by leagueID) as teamNo
+  WHERE city = "Swansea" and county = "Swansea"  and country = "Wales" and Sport = "Tennis" and teamNo.noTeams <= League.MaxTeams and teamNo.LeagueID = League.LeagueID;
 */
