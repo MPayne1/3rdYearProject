09edf710342a0cdc64ff3c8b8e5b32646d013ea5
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-02-08 08:11:16 +0000

    updated createLeague table;

--- Server/db/createdb.js
+++ Server/db/createdb.js
@@ -26,7 +26,8 @@ async function createLeagueTable(err) {
 	if(err) throw err;
 	var sql = "CREATE TABLE League(LeagueID int NOT NULL AUTO_INCREMENT,"+
 	"LeagueName VARCHAR(20), LeagueAdmin int, Sport VARCHAR(30), maxTeams int, " +
-  "pointsForLoss int , pointsForDraw int, pointsForWin int, PRIMARY KEY (LeagueID), "+
+  "pointsForLoss int , pointsForDraw int, pointsForWin int, city VARCHAR(30),"+
+  " county VARCHAR(30), country VARCHAR(30), games int, PRIMARY KEY (LeagueID),"+
 	"FOREIGN KEY (LeagueAdmin) REFERENCES Users(UserID));";
 	await db.query(sql, function(err, result) {
 		if(err) throw err;
