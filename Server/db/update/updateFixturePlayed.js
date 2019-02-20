08ceb1de17ec3da447feccecb150d421159ab9ab
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-02-20 15:01:04 +0000

    update fixture played db

---
+++ Server/db/update/updateFixturePlayed.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const dbCon  = require('../connection.js');
+
+//insert a new user into the db
+var updateFixture  = async function(fixtureID, played) {
+    var sql = `UPDATE fixture SET played = ${mysql.escape(played)},
+    WHERE fixtureID = ${mysql.escape(fixtureID)};`;
+	  await dbCon.query(sql , (err, result) => {
+		    if(err) throw err;
+        console.log('fixture updated');
+    });
+}
+
+module.exports = updateFixture;
