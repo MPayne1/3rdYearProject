6f14c327859766d669ef69e4a9443ad44d905d53
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-02-20 17:25:37 +0000

    backend insert tennisresults

--- Server/league/results/update.js
+++ Server/league/results/update.js
@@ -11,6 +11,7 @@ const joi = require('joi');
 const dbSelectUpdateFixtureAdmin = require('../../db/selectUpdateFixtureAdmin.js');
 const dbInsertFootballResult = require('../../db/insert/insertFootballResults.js');
 const dbUpdateFixturePlayed = require('../../db/update/updateFixturePlayed.js');
+const dbInsertTennisResult = require('../../db/insert/insertTennisResults.js');
 
 // ------ Schemas ------
 
@@ -23,6 +24,23 @@ const updateFootballResultsSchema  = joi.object().keys({
   AwayGoalsScoredFT: joi.number().min(0).required(),
   MatchDescription: joi.string().regex(/^[\w\-\s]{0,300}$/).required(),
 });
+
+// schema for inserting american football results
+const updateTennisResultsSchema  = joi.object().keys({
+  FixtureID: joi.number().positive().required(),
+  HomePointsScoredS1: joi.number().min(0).max(7).required(),
+  AwayPointsScoredS1: joi.number().min(0).max(7).required(),
+  HomePointsScoredS2: joi.number().min(0).max(7).required(),
+  AwayPointsScoredS2: joi.number().min(0).max(7).required(),
+  HomePointsScoredS3: joi.number().min(0).required(),
+  AwayPointsScoredS3: joi.number().min(0).required(),
+  HomePointsScoredS4: joi.number().min(0).max(7).required(),
+  AwayPointsScoredS4: joi.number().min(0).max(7).required(),
+  HomePointsScoredS5: joi.number().min(0).required(),
+  AwayPointsScoredS5: joi.number().min(0).required(),
+  MatchDescription: joi.string().regex(/^[\w\-\s]{0,300}$/).required(),
+});
+
 
 // schema for inserting american football results
 const updateAmericanFootballResultsSchema  = joi.object().keys({
@@ -50,7 +68,6 @@ router.get('/', (req, res) => {
 // handle request to insert football results
 router.post('/football', async(req, res, next) => {
   var userID = req.user.UserID;
-  console.log(userID);
   var fixtureID  = req.body.FixtureID;
   var HomeGoalsScoredHT = req.body.HomeGoalsScoredHT;
   var AwayGoalsScoredHT = req.body.AwayGoalsScoredHT;
@@ -86,13 +103,59 @@ router.post('/football', async(req, res, next) => {
 });
 
 // american Football results
-router.post('/americanFootball', async(req,res, next) => {
+router.post('/americanFootball', async(req, res, next) => {
   var userID = req.user.UserID;
-  var fixtureID  = req.body.FixtureID;
+  var fixtureID = req.body.FixtureID;
 })
 
 
 // handle req to insert tennis results
+router.post('/tennis', async(req, res, next) => {
+  const result = joi.validate(req.body, updateTennisResultsSchema);
+  if(result.error === null) {
+    var userID = req.user.UserID;
+    var fixtureID = req.body.FixtureID;
+    var HomePointsScoredS1 = req.body.HomePointsScoredS1;
+    var AwayPointsScoredS1 = req.body.AwayPointsScoredS1;
+    var HomePointsScoredS2 = req.body.HomePointsScoredS2;
+    var AwayPointsScoredS2 = req.body.AwayPointsScoredS2;
+    var HomePointsScoredS3 = req.body.HomePointsScoredS3;
+    var AwayPointsScoredS3 = req.body.AwayPointsScoredS3;
+    var HomePointsScoredS4 = req.body.HomePointsScoredS4;
+    var AwayPointsScoredS4 = req.body.AwayPointsScoredS4;
+    var HomePointsScoredS5 = req.body.HomePointsScoredS5;
+    var AwayPointsScoredS5 = req.body.AwayPointsScoredS5;
+    var MatchDescription = req.body.MatchDescription;
+
+    // check user is admin/captain
+    var update = await dbSelectUpdateFixtureAdmin(userID, fixtureID, async function(err, result) {
+      if(err) next(err);
+      // if result doesn't have league/team Admin then user can't update resuts
+      try {
+        result[0].leagueAdmin;
+        // if user is allowed, update the results
+        updateFixturePlayed(fixtureID);
+        var insert = await dbInsertTennisResult(fixtureID, HomePointsScoredS1
+          , AwayPointsScoredS1, HomePointsScoredS2, AwayPointsScoredS2
+          , HomePointsScoredS3, AwayPointsScoredS3, HomePointsScoredS4
+          , AwayPointsScoredS4,  HomePointsScoredS5, AwayPointsScoredS5
+          ,MatchDescription, (err) => {
+            if(err) next(err);
+        });
+        res.json(req.body);
+      } catch(e) {
+        var error = new Error("Only league admins and team captains can update reuslts");
+        res.status(403);
+        next(error);
+      }
+    });
+  }
+  else {
+    next(result.error);
+  }
+});
+
+
 
 // Rugby
 // Tennis
