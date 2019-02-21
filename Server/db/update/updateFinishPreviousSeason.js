65c2044847299fd5d2a4272bfff10351aaabb473
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-02-21 10:53:28 +0000

    updateseason finished

---
+++ Server/db/update/updateFinishPreviousSeason.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const dbCon  = require('../connection.js');
+
+// update season to be finished
+var updateSeasonFinished  = async function(leagueID, finished) {
+    var sql = `UPDATE season SET finished = ${mysql.escape(finished)}
+    WHERE leagueID = ${mysql.escape(leagueID)};`;
+	  await dbCon.query(sql , (err, result) => {
+		    if(err) callback(err);
+        console.log('season updated');
+    });
+}
+
+module.exports = updateSeasonFinished;
