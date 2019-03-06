29296f3f65d85c16424c2ecf8aae96572a6d85a3
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-06 19:14:55 +0000

    db update cricket rankings

---
+++ Server/db/update/rankings/updateCricketRankings.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const dbCon  = require('../../connection.js');
+
+//insert a new user into the db
+var updateCricketRanking = async function(seasonID, teamID, wins , draw, loss,
+  RunsFor, WicketsFor, RunsAgainst, WicketsAgainst, points) {
+    var sql = `UPDATE CricketRankings SET played = played + 1,
+    wins = wins + ${mysql.escape(wins)}, draws = draws + ${mysql.escape(draw)},
+    losses = losses + ${mysql.escape(loss)},
+    RunsFor = RunsFor + ${mysql.escape(RunsFor)},
+    WicketsFor = WicketsFor + ${mysql.escape(WicketsFor)},
+    RunsAgainst = RunsAgainst + ${mysql.escape(RunsAgainst)},
+    WicketsAgainst = WicketsAgainst + ${mysql.escape(WicketsAgainst)},
+    Points = Points + ${mysql.escape(points)}
+    WHERE seasonID = ${mysql.escape(seasonID)} and teamID = ${mysql.escape(teamID)};`;
+	  await dbCon.query(sql , (err, result) => {
+		    if(err) throw err;
+        console.log('cricket ranking updated');
+    });
+}
+
+module.exports = updateCricketRanking;
