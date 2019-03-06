ed7026aa58fb248247177822aca42f001c45e9ae
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-06 12:03:15 +0000

    db insert rugby ranking

---
+++ Server/db/insert/rankings/insertRugbyRankings.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const dbCon  = require('../../connection.js');
+
+//insert a new ranking in db, for initialising ranking for the season
+var insertRugbyRankings  = async function(seasonID, teamID, played,
+  wins, draws, losses, PointsScored, PointsConceded, Points) {
+    var sql = `INSERT INTO rugbyRankings(seasonID, TeamID, played,
+      wins, draws, losses, PointsScored, PointsConceded, Points)
+      VALUES(${mysql.escape(seasonID)}, ${mysql.escape(teamID)},
+       ${mysql.escape(played)},  ${mysql.escape(wins)},
+       ${mysql.escape(draws)},  ${mysql.escape(losses)},
+       ${mysql.escape(PointsScored)},  ${mysql.escape(PointsConceded)},
+       ${mysql.escape(Points)});`;
+	  await dbCon.query(sql , (err, result) => {
+		    if(err) throw(err);
+        console.log('ranking added');
+    });
+}
+
+module.exports = insertRugbyRankings;
