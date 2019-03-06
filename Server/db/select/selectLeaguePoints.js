d2bdc65664c6743db1924ad8f19ad6a923092729
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-06 14:48:58 +0000

    select points allocation for league from fixture id

---
+++ Server/db/select/selectLeaguePoints.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const dbCon  = require('../connection.js');
+
+/*
+  select the points allocation for the league, from the fixtureID
+*/
+var selectLeagueAdmin = async function(fixtureID, callback) {
+  var res;
+    var sql = `SELECT pointsForWin, pointsForDraw, pointsForLoss FROM league,fixture WHERE fixtureID = ${mysql.escape(fixtureID)}
+    and fixture.leagueID = league.leagueID`;
+	  await dbCon.query(sql , (err, result, fields) => {
+		    if(err) throw err;
+        res = result;
+        callback(null, res);
+    });
+}
+
+module.exports = selectLeagueAdmin;
