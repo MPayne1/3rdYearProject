3e696bffb4c455aabcec41269d876eb156c8395c
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-07 09:41:48 +0000

    get football rankings route

--- Server/league/rankings/fetch.js
+++ Server/league/rankings/fetch.js
@@ -7,6 +7,18 @@ const router = express.Router();
 const joi = require('joi');
 
 
+// ------  db operations  ------
+const dbFootballRankings = require('../../db/select/rankings/selectFootballRankings.js');
+
+// ------  schemas  ------
+
+// ranking schema
+const getRankingSchema = joi.object().keys({
+  leagueID: joi.number().positive().required()
+});
+
+// ------  routes  ------
+
 // all paths are prepended with /league/rankings/fetch
 router.get('/', (req, res) => {
   res.json({
@@ -14,7 +26,31 @@ router.get('/', (req, res) => {
   });
 });
 
+// route to get football rankings
+router.post('/football', async (req, res, next) => {
+  const result = joi.validate(req.body, getRankingSchema);
 
+  if(result.error === null) {
+    var leagueID = req.body.leagueID;
+    var rankings = await dbFootballRankings(leagueID, (err, result) => {
+      if(err) next (err);
+      try {
+        result[0];
+        res.json(result);
+      } catch (e) {
+        invalidInput(res, next)
+      }
+    });
+  } else {
+    next(result.error)
+  }
+});
 
+// create/format response for invalid inputs
+function invalidInput(res, next) {
+  res.status(409);
+  var error = new Error("Invlaid Input");
+  next(error);
+}
 
 module.exports = router;
