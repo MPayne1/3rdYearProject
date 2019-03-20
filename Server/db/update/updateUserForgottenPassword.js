dd378505a89123f1a2add3832e7cb8248381e025
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-20 16:17:34 +0000

    db update reset password info

---
+++ Server/db/update/updateUserForgottenPassword.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const dbCon  = require('../connection.js');
+
+// update users email
+var updateUserEmail  = async function(userID, token, expires) {
+    var sql = `UPDATE resetpassword SET resetToken = ${mysql.escape(token)},
+    resetExpires = ${mysql.escape(expires)}
+    WHERE userID = ${mysql.escape(userID)};`;
+	  await dbCon.query(sql , (err, result) => {
+		    if(err) next(err);
+        console.log('resetpassword info updated');
+    });
+}
+
+module.exports = updateUserEmail;
--- Server/db/update/updateUserPassword.js
+++ Server/db/update/updateUserPassword.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const dbCon  = require('../connection.js');
 
-// update users email
-var updateUserPassword  = async function(userID, password) {
+// update users password
+var updateUserEmail  = async function(userID, password) {
     var sql = `UPDATE users SET password = ${mysql.escape(password)}
     WHERE userID = ${mysql.escape(userID)};`;
 	  await dbCon.query(sql , (err, result) => {
@@ -11,4 +11,4 @@ var updateUserPassword  = async function(userID, password) {
     });
 }
 
-module.exports = updateUserPassword;
+module.exports = updateUserEmail;
