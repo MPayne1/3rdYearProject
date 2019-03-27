4460e7f43cec8e22676a3bb7a22d3f5f6efe8a4f
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-27 09:32:03 +0000

    updated insert user table

--- Server/db/insert/insertUser.js
+++ Server/db/insert/insertUser.js
@@ -2,11 +2,13 @@ const mysql = require('mysql');
 const dbCon  = require('../connection.js');
 
 //insert a new user into the db
-var insertUser  = async function(username, password, LastName, FirstName, Email) {
+var insertUser  = async function(username, password, LastName, FirstName, Email, phoneNumber, bio, publiclyShow) {
   var res;
-    var sql = `INSERT INTO users(username, password, LastName, FirstName, Email)
+    var sql = `INSERT INTO users(username, password, LastName, FirstName, Email, phoneNumber, bio, publiclyShow)
       VALUES(${mysql.escape(username)}, ${mysql.escape(password)}, ${mysql.escape(LastName)},
-      ${mysql.escape(FirstName)}, ${mysql.escape(Email)}); `;
+      ${mysql.escape(FirstName)}, ${mysql.escape(Email)},
+      ${mysql.escape(phoneNumber)}, ${mysql.escape(bio)},
+      ${mysql.escape(publiclyShow)}); `;
 	  await dbCon.query(sql , (err, result) => {
 		    if(err) throw err;
         console.log('user added');
