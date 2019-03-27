e68d6223ed93cfc6b786e1082f338560e4405772
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-27 13:58:53 +0000

    only show profile info if allowed, only show edit if logged in as same user, change bio, phonenumber and privacy info

--- client/src/views/playerInfo.vue
+++ client/src/views/playerInfo.vue
@@ -4,20 +4,72 @@
       <h2>Player Information</h2>
       <h4>{{username}}</h4>
     </div>
-    <div v-if="showSettings == false" class="alert alert-warning" role="alert">
+    <div v-if="!showInfo" class="alert alert-warning" role="alert">
       <h4>{{username}} has no information to publicly show.</h4>
     </div>
 
-    <div class="text-center row">
-      <div class="col-md-4"></div>
-      <div v-if="showSettings" class="col-md-4">
+    <div v-if="showInfo" class="text-center row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
         <div id="settingsCard" class="card">
-          <div class="text-white card-header bg-primary"><h4>Settings</h4></div>
+          <div class="text-white card-header bg-primary"><h4>Information</h4></div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item d-flex justify-content-between align-items-center card-body">
                 <div  id="settingsItem" class="align-items-center">
-                  <h5 @click="emailOpen = !emailOpen">Change Email</h5>
-                  <div v-if="emailOpen"class="">
+                  <h5>Name: {{profile.FirstName}} {{profile.LastName}}</h5>
+                </div>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center card-body">
+                <div  id="settingsItem" class="align-items-center">
+                  <h5 @click="bioOpen = !bioOpen, openCloseLi()">Bio: {{profile.Bio}}</h5>
+                  <div v-if="bioOpen && showEditOptions == true"class="">
+                    <form  @submit.prevent="changeBio()">
+                    <div class="form-group">
+                      <div v-if="errorMessage" class="alert alert-danger" role="alert">
+                        {{errorMessage}}
+                      </div>
+                      <div v-if="successMessage" class="alert alert-success" role="alert">
+                        {{successMessage}}
+                      </div>
+                      <br>
+                      <input v-model="profile.Bio" type="text" class="form-control"
+                        placeholder="New Bio" required>
+                      <br>
+                      <button @click="changeBio()" class="btn btn-primary btn-lg"
+                        type="submit">Change Bio</button>
+                    </div>
+                  </form>
+                  </div>
+                </div>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center card-body">
+                <div  id="settingsItem" class="align-items-center">
+                  <h5 @click="phoneOpen = !phoneOpen, openCloseLi()">Phone No: {{profile.PhoneNumber}}</h5>
+                  <div v-if="phoneOpen && showEditOptions == true"class="">
+                    <form  @submit.prevent="changePhoneNo()">
+                    <div class="form-group">
+                      <div v-if="errorMessage" class="alert alert-danger" role="alert">
+                        {{errorMessage}}
+                      </div>
+                      <div v-if="successMessage" class="alert alert-success" role="alert">
+                        {{successMessage}}
+                      </div>
+                      <br>
+                      <input v-model="profile.PhoneNumber" type="text" class="form-control"
+                        placeholder="Phone Number" required>
+                      <br>
+                      <button @click="changePhoneNo()" class="btn btn-primary btn-lg"
+                        type="submit">Change Phone No.</button>
+                    </div>
+                  </form>
+                  </div>
+                </div>
+              </li>
+
+              <li class="list-group-item d-flex justify-content-between align-items-center card-body">
+                <div  id="settingsItem" class="align-items-center">
+                  <h5 @click="emailOpen = !emailOpen, openCloseLi()">Email: {{profile.Email}}</h5>
+                  <div v-if="emailOpen && showEditOptions == true"class="">
                     <form  @submit.prevent="changeEmail()">
                     <div class="form-group">
                       <div v-if="errorMessage" class="alert alert-danger" role="alert">
@@ -42,10 +94,10 @@
                   </div>
                 </div>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center card-body">
+              <li v-if="showEditOptions" class="list-group-item d-flex justify-content-between align-items-center card-body">
                 <div id="settingsItem" class="align-items-center">
-                  <h5 @click="passwordOpen = !passwordOpen">Change Password</h5>
-                  <div v-if="passwordOpen"class="">
+                  <h5 @click="passwordOpen = !passwordOpen, openCloseLi()">Change Password</h5>
+                  <div v-if="passwordOpen && showEditOptions == true" class="">
                     <form  @submit.prevent="changePassword()">
                     <div class="form-group">
                       <div v-if="errorMessage" class="alert alert-danger" role="alert">
@@ -73,6 +125,32 @@
                   </div>
                 </div>
               </li>
+              <li v-if="showEditOptions" class="list-group-item d-flex justify-content-between align-items-center card-body">
+                <div id="settingsItem" class="align-items-center">
+                  <h5 @click="publiclyShowOpen = !publiclyShowOpen, openCloseLi()">Change Privacy</h5>
+                  <div v-if="publiclyShowOpen && showEditOptions == true" class="">
+                    <h6>Change if your profile information, name, bio, phone number and email is publicly shown to anyone logged into Sportsbook.</h6>
+                    <form  @submit.prevent="changePubliclyShown()">
+                    <div class="form-group">
+                      <div v-if="errorMessage" class="alert alert-danger" role="alert">
+                        {{errorMessage}}
+                      </div>
+                      <div v-if="successMessage" class="alert alert-success" role="alert">
+                        {{successMessage}}
+                      </div>
+                      <div class="form-group">
+                         <div class="custom-control custom-switch">
+                          <input type="checkbox" v-model="profile.publiclyShow"class="custom-control-input"  id="toggleSwitch" checked="">
+                          <label class="custom-control-label" for="toggleSwitch">Show your profile information publicly</label>
+                         </div>
+                       </div>
+                      <button class="btn btn-primary btn-lg"
+                        type="submit">Change Privacy</button>
+                    </div>
+                  </form>
+                  </div>
+                </div>
+              </li>
             </ul>
         </div>
       </div>
@@ -87,6 +165,10 @@
   const API_URL = 'https://localhost:3000/';
   const CHANGE_EMAIL_URL = 'https://localhost:3000/auth/changeEmail';
   const CHANGE_PASSWORD_URL = 'https://localhost:3000/auth/changePassword';
+  const VIEW_PROFILE_URL = 'https://localhost:3000/player/profile/view';
+  const CHANGE_BIO_URL = 'https://localhost:3000/player/profile/changeInfo/Bio';
+  const CHANGE_PHONE_NO_URL = 'https://localhost:3000/player/profile/changeInfo/phoneNumber';
+  const CHANGE_PUBLICLY_SHOW_URL = 'https://localhost:3000/player/profile/changeInfo/publiclyShow';
 
   const changeEmailSchema = joi.object().keys({
     username: joi.string().alphanum().min(2).max(20)
@@ -102,9 +184,14 @@
     confirmPassword: joi.string().trim().min(8).required(),
   });
 
+  const viewProfileSchema = joi.object().keys({
+    username: joi.string().alphanum().min(2).max(20).required()
+  });
+
   export default {
     data: () => ({
       user: {},
+      profile: {},
       email: {
         email: '',
         username: '',
@@ -122,6 +209,11 @@
       showSettings: false,
       emailOpen: false,
       passwordOpen: false,
+      bioOpen: false,
+      phoneOpen: false,
+      publiclyShowOpen: false,
+      showEditOptions: false,
+      showInfo: false,
     }),
     watch: {
       email: {
@@ -135,6 +227,7 @@
       // get the username
       if (this.$route.params.username) {
         this.username = this.$route.params.username;
+        this.getProfileInfo();
       } else {
         this.$router.push('/dashboard/');
       }
@@ -154,11 +247,118 @@
             this.$router.push('/auth/login');
           }
           if(this.user.username === this.username) {
-            this.showSettings = true;
+            this.showEditOptions = true;
           }
         });
+
+
     },
     methods: {
+
+      openCloseLi() {
+        this.successMessage = '';
+      },
+      getProfileInfo() {
+          const body = {
+            username: this.username,
+          };
+          fetch(VIEW_PROFILE_URL, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+              'content-type': 'application/json',
+              Authorization: `Bearer ${localStorage.token}`,
+            },
+          }).then(response => response.json())
+          .then((res) =>{
+            if (res.message) {
+              this.showInfo = false;
+            } else{
+              this.profile = res[0];
+              this.showInfo = true;
+            }
+            console.log(res);
+          }).catch((error) => { // if any errors catch them any display error message
+            this.errorMessage = error.message;
+          });
+      },
+      changePubliclyShown() {
+        var show = '';
+        console.log(this.profile.publiclyShow);
+        if(this.profile.publiclyShow == false) {
+          show = 'False';
+        } else {
+          show = 'True';
+        }
+        const body = {
+          publiclyShow: show,
+        }
+        // send the request to the backend
+        fetch(CHANGE_PUBLICLY_SHOW_URL, {
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        }).then(response => response.json())
+        .then((res) =>{
+          if (res.message) {
+            this.successMessage = res.message;
+          }
+          console.log(res);
+        }).catch((error) => { // if any errors catch them any display error message
+          this.errorMessage = error.message;
+        });
+      },
+      changePhoneNo() {
+        if(this.profile.PhoneNumber != '') {
+          const body = {
+            phoneNumber: this.profile.PhoneNumber,
+          };
+          // send the request to the backend
+          fetch(CHANGE_PHONE_NO_URL, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+              'content-type': 'application/json',
+              Authorization: `Bearer ${localStorage.token}`,
+            },
+          }).then(response => response.json())
+          .then((res) =>{
+            if (res.message) {
+              this.successMessage = res.message;
+            }
+            console.log(res);
+          }).catch((error) => { // if any errors catch them any display error message
+            this.errorMessage = error.message;
+          });
+        }
+      },
+      changeBio() {
+        if(this.profile.Bio != '') {
+          const body = {
+            Bio: this.profile.Bio,
+          };
+          // send the request to the backend
+          fetch(CHANGE_BIO_URL, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+              'content-type': 'application/json',
+              Authorization: `Bearer ${localStorage.token}`,
+            },
+          }).then(response => response.json())
+          .then((res) =>{
+            if (res.message) {
+              this.successMessage = res.message;
+            }
+            console.log(res);
+          }).catch((error) => { // if any errors catch them any display error message
+            this.errorMessage = error.message;
+          });
+        }
+      },
       changeEmail() {
         if (this.validEmail()) {
           const body = {
@@ -250,6 +450,17 @@
           this.errorMessage = 'Email is invalid, please enter a valid email address';
         } else {
           this.errorMessage = 'Password is invalid, must be at least 8 characters';
+        }
+        return false;
+      },
+
+      validProfileInfo() {
+        const result = joi.validate(this.username, viewProfileSchema);
+        if(result.error === null) {
+          return true;
+        }
+        if(result.error.message.includes('username')){
+          this.errorMessage = 'Please refresh the page';
         }
         return false;
       },
