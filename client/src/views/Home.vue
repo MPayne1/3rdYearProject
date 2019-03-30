994985decb747ea46e19a2fb1deac7c0bb2cf700
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-30 09:05:14 +0000

    homepage more info

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="home text-center">
+  <div class="home">
     <div class="jumbotron">
-      <h1 class="display-3">SportsBook</h1>
-      <p class="lead">SportsBook is an easy to use web application to
-        manage your amateur sports team.</p>
+      <div class="text-center">
+        <h1 class="display-3">SportsBook</h1>
+        <p class="lead">SportsBook is an easy to use web application to
+          manage your amateur sports team.</p>
+      </div>
+      <div class="text-center">
+
+      </div>
+      <h5>You can:</h5>
+      <ul>
+        <li>Create a team/league for a range of sports</li>
+        <li>Automatically generate fixtures</li>
+        <li>Easily record match results</li>
+        <li>Automatically update the league table</li>
+      </ul>
       <hr class="my-4">
-      <p>Sign up and start today!</p>
-      <p class="lead">
-        <router-link id="signupBtn" class="btn btn-primary btn-lg" :to="{name: 'signup'}"
-          role="button">Sign Up</router-link>
-        <router-link id="loginBtn" class="btn btn-primary btn-lg" :to="{name: 'login'}"
-          role="button">Login</router-link>
-      </p>
+      <div class="text-center">
+        <h5>Sign up and start today!</h5>
+        <p class="lead">
+          <router-link id="signupBtn" class="btn btn-primary btn-lg" :to="{name: 'signup'}"
+            role="button">Sign Up</router-link>
+          <router-link id="loginBtn" class="btn btn-primary btn-lg" :to="{name: 'login'}"
+            role="button">Login</router-link>
+        </p>
+      </div>
     </div>
   </div>
 </template>
