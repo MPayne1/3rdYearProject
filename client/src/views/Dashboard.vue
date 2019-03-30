3f51542808e8b6d4766bf80abb19ede79a62986e
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-03-30 10:13:17 +0000

    frontend show all users league/team announcements

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -9,21 +9,48 @@
       <button v-if="!loggedIn" @click="login()" class="btn btn-primary btn-lg"
         type="submit">Login</button>
     </div>
-
-    <div class="jumbotron">
-      <h3>You have the following announcements:</h3>
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+        <div id="announcementCard" class="card  bg-secondary">
+          <div id="announcementList" class="card-header text-white"><h4>Your League Announcements</h4></div>
+          <ul class="list-group list-group-flush text-center">
+            <li class="list-group-item d-flex justify-content-between align-items-center card-body text-center"
+            v-for="announcement in LeagueAnnouncements" @click="goToLeaguePage(announcement.leaguename)">
+              <h5><b>{{announcement.leaguename}}: </b>{{announcement.message}}</h5>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+        <div id="announcementCard" class="card  bg-secondary">
+          <div id="announcementList" class="card-header text-white"><h4>Your Team Announcements</h4></div>
+          <ul class="list-group list-group-flush text-center">
+            <li class="list-group-item d-flex justify-content-between align-items-center card-body text-center"
+            v-for="announcement in TeamAnnouncements" @click="goToTeamPage(announcement.teamname)">
+              <h5><b>{{announcement.teamname}}:</b> {{announcement.message}}</h5>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 const API_URL = 'https://localhost:3000/';
+const GET_LEAGUE_ANNOUNCEMENT_URL = 'https://localhost:3000/player/dashboard/LeagueAnnouncements';
+const GET_TEAM_ANNOUNCEMENT_URL = 'https://localhost:3000/player/dashboard/TeamAnnouncements';
+
 export default {
   data: () => ({
     user: {},
     loggedIn: false,
+    LeagueAnnouncements: {},
+    TeamAnnouncements: {},
   }),
   mounted() {
     // get the authorization header
@@ -43,8 +70,50 @@ export default {
           this.$router.push('/auth/login');
         }
       });
+      this.getLeagueAnnouncements();
+      this.getTeamAnnouncements();
   },
   methods: {
+    getLeagueAnnouncements() {
+      fetch(GET_LEAGUE_ANNOUNCEMENT_URL, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      }).then(res => res.json())
+        .then((result) => {
+          if (result.message) {
+            this.announcementErrorMessage = result.message;
+          } else {
+            console.log(result);
+            this.LeagueAnnouncements = result;
+          }
+        });
+    },
+    getTeamAnnouncements() {
+      fetch(GET_TEAM_ANNOUNCEMENT_URL, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      }).then(res => res.json())
+        .then((result) => {
+          if (result.message) {
+            this.announcementErrorMessage = result.message;
+          } else {
+            console.log(result);
+            this.TeamAnnouncements = result;
+          }
+        });
+    },
+    goToLeaguePage(leagueName) {
+        this.$router.push({ path: '/league/info', query: { leagueName } });
+    },
+    goToTeamPage(teamName) {
+      this.$router.push({ path: '/team/info', query: { teamName } });
+    },
     logout() {
       localStorage.removeItem('token');
       location.reload();
