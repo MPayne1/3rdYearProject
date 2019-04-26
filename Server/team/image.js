b38ffb3af6a3cb8552bc1028a9aec8be81f952c1
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-04-26 09:23:51 +0100

    update team image route

--- Server/team/image.js
+++ Server/team/image.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const joi = require('joi');
 
 const dbSelectTeamImage = require('../db/select/Team/selectTeamImage.js');
-
+const dbUpdateImage = require('../db/update/updateTeamImage.js');
 
 // all paths are prepended with /team/image
 router.get('/', (req, res) => {
@@ -28,6 +28,36 @@ router.get('/teamImage', async(req, res, next) => {
   });
 });
 
+// handle the request to change the teams picture
+router.post('/newImage', async(req, res, next) => {
+  try{
+    var file = req.files.teamImage;
+    var imageName = req.files.teamImage.name;
+    // check file size smaller than 10MB
+    if(file.size < 10000000) {
+      // check file type
+      if(file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/jpg") {
+        // add random string to front of name to prevent duplicate names
+        var nameSalt = crypto.randomBytes(20).toString('hex');
+        imageName = nameSalt + imageName;
 
+        file.mv('Client/src/assets/Team Images/' + imageName, async(err) => {
+          if(err) next(err);
+          await dbUpdateImage(req.body.TeamID, imageName);
+
+          console.log(`team image ${imageName} uploaded`);
+          res.json({message: "Team Image Changed", imagePath: imageName});
+        });
+      } else {
+        throw new Error();
+      }
+    } else {
+      throw new Error();
+    }
+  } catch(e){
+    res.status(422);
+    res.json({errorMessage: "Please upload an image in jgp, jpeg or png format smaller than 10MB"})
+  }
+});
 
 module.exports = router;
