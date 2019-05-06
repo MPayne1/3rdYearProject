303831bfe5fe3b0bd98c7a43748b226e2826548e
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2019-05-06 13:04:47 +0100

    added if not exists to all create tables, combined all commented out calls into one funciton

--- Server/db/createdb.js
+++ Server/db/createdb.js
@@ -12,7 +12,7 @@ console.log(dotenv.parsed)
 // create users table
 async function createUsersTable(err) {
 	if(err) throw err;
-	var sql = "CREATE TABLE users(UserID int NOT NULL AUTO_INCREMENT, " +
+	var sql = "CREATE TABLE IF NOT EXISTS users(UserID int NOT NULL AUTO_INCREMENT, " +
 	"username VARCHAR(20), password VARCHAR(255), LastName VARCHAR(255),"+
 	" FirstName VARCHAR(255), Email VARCHAR(50), Bio VARCHAR(200), "+
   "PhoneNumber VARCHAR(15), publiclyShow ENUM('False', 'True'), " +
@@ -26,7 +26,7 @@ async function createUsersTable(err) {
 // create League table
 async function createLeagueTable(err) {
 	if(err) throw err;
-	var sql = "CREATE TABLE League(LeagueID int NOT NULL AUTO_INCREMENT,"+
+	var sql = "CREATE TABLE IF NOT EXISTS  League(LeagueID int NOT NULL AUTO_INCREMENT,"+
 	"LeagueName VARCHAR(20), LeagueAdmin int, Sport VARCHAR(30), maxTeams int, " +
   "pointsForLoss int , pointsForDraw int, pointsForWin int, city VARCHAR(30),"+
   " county VARCHAR(30), country VARCHAR(30), games int, leagueDescription VARCHAR(300)" +
@@ -40,7 +40,7 @@ async function createLeagueTable(err) {
 // create teams table
 async function createTeamTable(err) {
 	if(err) throw err;
-	var sql = "CREATE TABLE Team(TeamID int NOT NULL AUTO_INCREMENT,"+
+	var sql = "CREATE TABLE IF NOT EXISTS Team(TeamID int NOT NULL AUTO_INCREMENT,"+
 	"TeamName VARCHAR(20), TeamAdmin int,LeagueID int, teamDescription VARCHAR(300) "
    +", imagePath VARCHAR(255), PRIMARY KEY (TeamID), "+
 	"FOREIGN KEY (LeagueID) REFERENCES League(LeagueID), "+
@@ -54,7 +54,7 @@ async function createTeamTable(err) {
 // create Plays for Table
 async function createPlaysForTable(err) {
 	if(err) throw err;
-	var sql = "CREATE TABLE PlaysFor(TeamID int, UserID int, " +
+	var sql = "CREATE TABLE IF NOT EXISTS PlaysFor(TeamID int, UserID int, " +
 	"PRIMARY KEY (TeamID, UserID), " +
 	"FOREIGN KEY (UserID) REFERENCES Users(UserID), " +
 	"FOREIGN KEY (TeamID) REFERENCES Team(TeamID));";
@@ -67,7 +67,7 @@ async function createPlaysForTable(err) {
 // create fixtures table
 async function createFixturesTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE FIXTURE(FixtureID int NOT NULL AUTO_INCREMENT" +
+  var sql = "CREATE TABLE IF NOT EXISTS FIXTURE(FixtureID int NOT NULL AUTO_INCREMENT" +
     " , SeasonID int, HomeTeamID int, AwayTeamID int, date DATE" +
     ", startTime VARCHAR(30), endTime VARCHAR(30)" +
     ", address VARCHAR(30), city VARCHAR(30), county VARCHAR(30), postcode VARCHAR(30) " +
@@ -86,7 +86,7 @@ async function createFixturesTable(err) {
 // create season table
 async function createSeasonsTable(err) {
 	if(err) throw err;
-	var sql = "CREATE TABLE Season(SeasonID int NOT NULL AUTO_INCREMENT," +
+	var sql = "CREATE TABLE IF NOT EXISTS Season(SeasonID int NOT NULL AUTO_INCREMENT," +
     "LeagueID int, Finished  ENUM('false', 'true') NOT NULL DEFAULT 'false'," +
 	  "PRIMARY KEY (SeasonID), " +
 	  "FOREIGN KEY (LeagueID) REFERENCES League(LeagueID));";
@@ -101,7 +101,7 @@ async function createSeasonsTable(err) {
 // create Football result table
 async function createFootballResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE FootballResults(FixtureID int, HomeGoalsScoredHT int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS FootballResults(FixtureID int, HomeGoalsScoredHT int,"+
   " AwayGoalsScoredHT int,HomeGoalsScoredFT int, AwayGoalsScoredFT int, "+
   "MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID),"+
   " FOREIGN KEY (FixtureID) REFERENCES Fixture(FixtureID));";
@@ -114,7 +114,7 @@ async function createFootballResultsTable(err) {
 // create Rugby result table
 async function createRugbyResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE RugbyResults(FixtureID int, HomePointsScoredHT int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS RugbyResults(FixtureID int, HomePointsScoredHT int,"+
   " AwayPointsScoredHT int,HomePointsScoredFT int, AwayPointsScoredFT int, " +
   "MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID), "+
   "FOREIGN KEY (FixtureID) REFERENCES Fixture(FixtureID));";
@@ -127,7 +127,7 @@ async function createRugbyResultsTable(err) {
 // create Rugby result table
 async function createAmericanFootballResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE AmericanFootballResults(FixtureID int, HomePointsScoredQ1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS AmericanFootballResults(FixtureID int, HomePointsScoredQ1 int,"+
   " AwayPointsScoredQ1 int, HomePointsScoredHT int, AwayPointsScoredHT int,"+
   " HomePointsScoredQ3 int, AwayPointsScoredQ3 int, HomePointsScoredFT int, "+
   "AwayPointsScoredFT int, MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID), "+
@@ -141,7 +141,7 @@ async function createAmericanFootballResultsTable(err) {
 // create basketball results table
 async function createBasketballResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE BasketballResults(FixtureID int, HomePointsScoredQ1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS BasketballResults(FixtureID int, HomePointsScoredQ1 int,"+
   " AwayPointsScoredQ1 int, HomePointsScoredHT int, AwayPointsScoredHT int,"+
   " HomePointsScoredQ3 int, AwayPointsScoredQ3 int, HomePointsScoredFT int, "+
   "AwayPointsScoredFT int, MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID), "+
@@ -155,7 +155,7 @@ async function createBasketballResultsTable(err) {
 // create tennis results table
 async function createTennisResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE TennisResults(FixtureID int, HomePointsScoredS1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS TennisResults(FixtureID int, HomePointsScoredS1 int,"+
   " AwayPointsScoredS1 int, HomePointsScoredS2 int, AwayPointsScoredS2 int,"+
   " HomePointsScoredS3 int, AwayPointsScoredS3 int, HomePointsScoredS4 int, "+
   "AwayPointsScoredS4 int, HomePointsScoredS5 int, AwayPointsScoredS5 int, " +
@@ -171,7 +171,7 @@ async function createTennisResultsTable(err) {
 // create cricket results table
 async function createCricketResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE CricketResults(FixtureID int, HomeRunsI1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS CricketResults(FixtureID int, HomeRunsI1 int,"+
   " AwayRunsI1 int, HomeWicketsLostI1 int, AwayWicketsLostI1 int,"+
   " HomeRunsI2 int, AwayRunsI2 int, HomeWicketsLostI2 int, "+
   "AwayWicketsLostI2 int, MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID),"+
@@ -185,7 +185,7 @@ async function createCricketResultsTable(err) {
 // create volleyball results table
 async function createVolleyballResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE VolleyballResults(FixtureID int, HomePointsScoredG1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS VolleyballResults(FixtureID int, HomePointsScoredG1 int,"+
   " AwayPointsScoredG1 int, HomePointsScoredG2 int, AwayPointsScoredG2 int,"+
   " HomePointsScoredG3 int, AwayPointsScoredG3 int, HomePointsScoredG4 int, "+
   "AwayPointsScoredG4 int, HomePointsScoredG5 int, AwayPointsScoredG5 int, " +
@@ -200,7 +200,7 @@ async function createVolleyballResultsTable(err) {
 // create hockey result table
 async function createHockeyResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE HockeyResults(FixtureID int, HomePointsScoredHT int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS HockeyResults(FixtureID int, HomePointsScoredHT int,"+
   " AwayPointsScoredHT int,HomePointsScoredFT int, AwayPointsScoredFT int, " +
   "MatchDescription VARCHAR(300), PRIMARY KEY (FixtureID), "+
   "FOREIGN KEY (FixtureID) REFERENCES Fixture(FixtureID));";
@@ -214,7 +214,7 @@ async function createHockeyResultsTable(err) {
 // create table tennis results table
 async function createTableTennisResultsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE TableTennisResults(FixtureID int, HomePointsScoredG1 int,"+
+  var sql = "CREATE TABLE IF NOT EXISTS TableTennisResults(FixtureID int, HomePointsScoredG1 int,"+
   " AwayPointsScoredG1 int, HomePointsScoredG2 int, AwayPointsScoredG2 int,"+
   " HomePointsScoredG3 int, AwayPointsScoredG3 int, HomePointsScoredG4 int, "+
   "AwayPointsScoredG4 int, HomePointsScoredG5 int, AwayPointsScoredG5 int, " +
@@ -231,7 +231,7 @@ async function createTableTennisResultsTable(err) {
 // create football rankings table
 async function createFootballRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE FootballRankings(SeasonID int, TeamID int,Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS FootballRankings(SeasonID int, TeamID int,Played int," +
   " Wins int, Draws int, Losses int, GoalsScored int, GoalsConceded int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -245,7 +245,7 @@ async function createFootballRankingsTable(err) {
 // create american football rankings table
 async function createAmericanFootballRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE AmericanFootballRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS AmericanFootballRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, PointsScored int, PointsConceded int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -259,7 +259,7 @@ async function createAmericanFootballRankingsTable(err) {
 // create rugby rankings table
 async function createRugbyRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE RugbyRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS RugbyRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, PointsScored int, PointsConceded int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -273,7 +273,7 @@ async function createRugbyRankingsTable(err) {
 // create basketball rankings table
 async function createBasketballRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE BasketballRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS BasketballRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, PointsScored int, PointsConceded int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -287,7 +287,7 @@ async function createBasketballRankingsTable(err) {
 // create tennis rankings table
 async function createTennisRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE TennisRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS TennisRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, SetsFor int, SetsAgainst int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -301,7 +301,7 @@ async function createTennisRankingsTable(err) {
 // create table tennis rankings table
 async function createTableTennisRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE TableTennisRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS TableTennisRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, SetsFor int, SetsAgainst int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -315,7 +315,7 @@ async function createTableTennisRankingsTable(err) {
 // create hockey rankings table
 async function createHockeyRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE HockeyRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS HockeyRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, GoalsScored int, GoalsConceded int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -329,7 +329,7 @@ async function createHockeyRankingsTable(err) {
 // create cricket rankings table
 async function createCricketRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE CricketRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS CricketRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, RunsFor int, WicketsFor int,"+
   " RunsAgainst int, WicketsAgainst int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
@@ -344,7 +344,7 @@ async function createCricketRankingsTable(err) {
 // create volleyball rankings table
 async function createVolleyballRankingsTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE VolleyballRankings(SeasonID int, TeamID int, Played int," +
+  var sql = "CREATE TABLE IF NOT EXISTS VolleyballRankings(SeasonID int, TeamID int, Played int," +
   " Wins int, Draws int, Losses int, GamesFor int, GamesAgainst int, Points int," +
   "PRIMARY KEY (SeasonID, TeamID)," +
   " FOREIGN KEY (SeasonID) REFERENCES season(SeasonID), " +
@@ -358,7 +358,7 @@ async function createVolleyballRankingsTable(err) {
 // create table for password reset
 async function createPasswordResetTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE passwordReset(UserID int, resetToken VARCHAR(40), "+
+  var sql = "CREATE TABLE IF NOT EXISTS passwordReset(UserID int, resetToken VARCHAR(40), "+
   "resetExpires int, PRIMARY KEY (UserID), "+
   "FOREIGN KEY(UserID) REFERENCES users(UserID));";
   await db.query(sql, (err, result) => {
@@ -370,7 +370,7 @@ async function createPasswordResetTable(err) {
 // create table for team announcements
 async function createTeamAnnouncementTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE teamAnnouncement(AnnouncementID int NOT NULL AUTO_INCREMENT,"+
+  var sql = "CREATE TABLE IF NOT EXISTS teamAnnouncement(AnnouncementID int NOT NULL AUTO_INCREMENT,"+
   "TeamID int, message VARCHAR(200), PRIMARY KEY(AnnouncementID),"+
   " FOREIGN KEY (TeamID) REFERENCES team(TeamID));";
   await db.query(sql, (err, result) => {
@@ -382,7 +382,7 @@ async function createTeamAnnouncementTable(err) {
 // create table for league announcements
 async function createLeagueAnnouncementTable(err) {
   if(err) throw err;
-  var sql = "CREATE TABLE leagueAnnouncement(LeagueAnnouncementID int NOT NULL AUTO_INCREMENT,"+
+  var sql = "CREATE TABLE IF NOT EXISTS leagueAnnouncement(LeagueAnnouncementID int NOT NULL AUTO_INCREMENT,"+
   "LeagueID int, message VARCHAR(200), PRIMARY KEY(LeagueAnnouncementID),"+
   " FOREIGN KEY (LeagueID) REFERENCES League(LeagueID));";
   await db.query(sql, (err, result) => {
@@ -392,38 +392,39 @@ async function createLeagueAnnouncementTable(err) {
 }
 
 
+// create all tables
+async function createDB() {
+  await createUsersTable();
+  await createLeagueTable();
+  await createTeamTable();
+  await createPlaysForTable();
+  await createSeasonsTable();
+  await createFixturesTable();
+  await createPasswordResetTable();
+  await createTeamAnnouncementTable();
+  await createLeagueAnnouncementTable();
 
+  // ------  create Results tables  ------
 
-// createUsersTable();
-// createLeagueTable();
-// createTeamTable();
-// createPlaysForTable();
-// createSeasonsTable();
-// createFixturesTable();
-// createPasswordResetTable();
-// createTeamAnnouncementTable();
-// createLeagueAnnouncementTable();
+  await createFootballResultsTable();
+  await createRugbyResultsTable();
+  await createAmericanFootballResultsTable();
+  await createBasketballResultsTable();
+  await createTennisResultsTable();
+  await createCricketResultsTable();
+  await createVolleyballResultsTable();
+  await createHockeyResultsTable();
+  await createTableTennisResultsTable();
 
-// ------  create Results tables  ------
+  // ------  create Rankings tables  ------
 
-// createFootballResultsTable();
-// createRugbyResultsTable();
-// createAmericanFootballResultsTable();
-// createBasketballResultsTable();
-// createTennisResultsTable();
-// createCricketResultsTable();
-// createVolleyballResultsTable();
-// createHockeyResultsTable();
-// createTableTennisResultsTable();
-
-// ------  create Rankings tables  ------
-
-// createFootballRankingsTable();
-// createAmericanFootballRankingsTable();
-// createRugbyRankingsTable();
-// createBasketballRankingsTable();
-// createTennisRankingsTable();
-// createTableTennisRankingsTable();
-// createHockeyRankingsTable();
-// createCricketRankingsTable();
-// createVolleyballRankingsTable();
+  await createFootballRankingsTable();
+  await createAmericanFootballRankingsTable();
+  await createRugbyRankingsTable();
+  await createBasketballRankingsTable();
+  await createTennisRankingsTable();
+  await createTableTennisRankingsTable();
+  await createHockeyRankingsTable();
+  await createCricketRankingsTable();
+  await createVolleyballRankingsTable();
+}
